ce1507fe4480502ec2d27f3ce8ac5dd65255b670
duclin <duclin@taiwanmobile.com>
2026-05-04 17:00:02 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+
+// 匯出你專屬的 Firebase 網頁應用程式設定
+export const firebaseConfig = {
+  apiKey: "AIzaSyDOnBVT-O-IRMKFBquzYLPsx9C94cMQ6AE",
+  authDomain: "twmgame-92186.firebaseapp.com",
+  databaseURL: "https://twmgame-92186-default-rtdb.firebaseio.com",
+  projectId: "twmgame-92186",
+  storageBucket: "twmgame-92186.firebasestorage.app",
+  messagingSenderId: "611240332765",
+  appId: "1:611240332765:web:5c3dc213be5c9c24f3ba34",
+  measurementId: "G-NXR1GP79HC"
+};
